99f8e81e82422e9bf90ceada621dede690f5008e
Rafael Sulimann <sulimannrafael@gmail.com>
2024-04-08 22:02:07 -0300

    feat: Email do autor único

--- src/main/java/com/sulimann/cleanarch/infra/controllers/autor/criar/CriarAutorRequest.java
+++ src/main/java/com/sulimann/cleanarch/infra/controllers/autor/criar/CriarAutorRequest.java
@@ -2,6 +2,8 @@ package com.sulimann.cleanarch.infra.controllers.autor.criar;
 
 import com.sulimann.cleanarch.core.constants.ErrorMessage;
 import com.sulimann.cleanarch.core.usecases.autor.criar.ICriarAutorRequest;
+import com.sulimann.cleanarch.infra.entities.AutorEntity;
+import com.sulimann.cleanarch.infra.utils.validators.uniquevalue.UniqueValue;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -16,6 +18,7 @@ public class CriarAutorRequest implements ICriarAutorRequest{
 
   @NotBlank(message = ErrorMessage.CAMPO_OBRIGATORIO)
   @Email(message = "Email inválido")
+  @UniqueValue(domainClass = AutorEntity.class, fieldName = "email", message = "Email já existente")
   private String email;
 
   @NotBlank(message = ErrorMessage.CAMPO_OBRIGATORIO)
